4a0ed69b6a03fe9545a9c787038934672d3a9732
Anjani Chapparapu <anjani.chapparapu@bas.no>
2025-06-29 11:16:08 +0530

    add projectile markers for player to spawn projectile correctly based on player direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,20 +3,35 @@ extends CharacterBody2D
 @export var projectile_scene: PackedScene
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var projectile_spawn_right = $PlayerProjectileSpawnRight
+@onready var projectile_spawn_left = $PlayerProjectileSpawnLeft
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var facing_direction := 1  # 1 = right, -1 = left
+
 func _input(event):
 	if event.is_action_pressed("shoot_player_projectile"):
 		shoot_projectile()
+	
+	if event.is_action_pressed("move_right"):
+		facing_direction = 1
+	
+	if(event.is_action_pressed("move_left")):
+		facing_direction = -1
+
 
 func shoot_projectile():
 	if projectile_scene:
 		var projectile = projectile_scene.instantiate()
 		
-		projectile.global_position = global_position
-		projectile.direction = Vector2.RIGHT
+		if facing_direction == 1:
+			projectile.global_position = projectile_spawn_right.global_position
+		else:
+			projectile.global_position = projectile_spawn_left.global_position
+			
+		projectile.direction = Vector2(facing_direction, 0)
 		get_tree().current_scene.add_child(projectile)
 
 func _physics_process(delta: float) -> void:
