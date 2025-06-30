3456ec5ea0f6f6be7f516684f959a9639abb48e1
TheMB04 <122024164+TheMB04@users.noreply.github.com>
2025-06-30 16:15:53 +0200

    notes

--- scripts/player_projectile.gd
+++ scripts/player_projectile.gd
@@ -10,5 +10,6 @@ func _physics_process(delta):
 func _on_area_entered(area):
 	queue_free()  # Remove bullet on hit 
 	
+# Removes projectile when off screen
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
