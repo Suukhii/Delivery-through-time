dafdf5fe300ad3d1fdffe83bde4e255ce74c9685
Suukhii <sukhmanikakar11@gmail.com>
2025-06-30 15:44:30 +0200

    added heart system (not modular)

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -9,13 +9,19 @@ var direction = 1
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
 		direction = -1
+		animated_sprite.flip_h = false
 		
 	if ray_cast_left.is_colliding():
 		direction = 1
-		animated_sprite.flip_h
+		animated_sprite.flip_h = true
 	position.x += direction * SPEED *delta
+
+
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":  
+		body.on_hit()
