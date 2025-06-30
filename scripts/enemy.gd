9e81ed7665e9abc15a3e88fb626bf14a0feb36d6
Suukhii <sukhmanikakar11@gmail.com>
2025-06-30 16:54:36 +0200

    added mummy animation

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -12,13 +12,13 @@ var direction = 1
 func _process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
 		direction = -1
-		animated_sprite.flip_h = false
+		animated_sprite.flip_h = true	
 		
 	if ray_cast_left.is_colliding():
 		direction = 1
-		animated_sprite.flip_h = true
+		animated_sprite.flip_h = false
 	position.x += direction * SPEED *delta
-
+	
 
 
 
