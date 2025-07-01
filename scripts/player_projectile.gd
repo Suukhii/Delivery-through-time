0b20aff682a176f13ebeb651bfc8f467815f09ad
Suukhii <sukhmanikakar11@gmail.com>
2025-07-01 11:34:43 +0200

    fixed enemy detection

--- scripts/player_projectile.gd
+++ scripts/player_projectile.gd
@@ -8,12 +8,12 @@ func _physics_process(delta):
 	position += direction * speed * delta
 
 func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("Projectile") or body.is_in_group("Player"): 
-		pass
-	else:
+	if body.is_in_group("Enemy"):
+		print("Hit Enemy: ", body.name)
+		queue_free()
+	elif not body.is_in_group("Projectile") and not body.is_in_group("Player"):
 		print("Hit: ", body.name)
-		queue_free()  # Remove bullet on hit 
-	
+		queue_free()
 # Removes projectile when off screen
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
