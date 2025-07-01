f92f08e43f605ffadd8360d572a690df69bd8aaa
TheMB04 <122024164+TheMB04@users.noreply.github.com>
2025-07-01 23:43:38 +0200

    Improved jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,7 +10,9 @@ extends CharacterBody2D
 signal hit
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
+const GRAVITY = 1000
+const FALL_GRAVITY = 1600
 
 var facing_direction := 1  # 1 = right, -1 = left
 var is_throwing := false
@@ -46,16 +48,26 @@ func shoot_projectile_with_throw() -> void:
 	get_tree().current_scene.add_child(projectile)
 
 	is_throwing = false
+	
+	
+func return_gravity(velocity: Vector2):
+	if velocity.y < 0:
+		return GRAVITY
+	return FALL_GRAVITY
 
 
 func _physics_process(delta: float) -> void:
 	# Add gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += return_gravity(velocity) * delta
 		
 	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	# Makes jump pressure sensitive
+	if Input.is_action_just_released("jump") and velocity.y < 0 and velocity.y < JUMP_VELOCITY/4:
+		velocity.y = JUMP_VELOCITY / 4
 
 	# Movement
 	var direction := Input.get_axis("move_left", "move_right")
