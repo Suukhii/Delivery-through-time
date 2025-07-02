38931460cc0d726d829620fe8274996a4d5680c5
TheMB04 <122024164+TheMB04@users.noreply.github.com>
2025-07-02 16:43:46 +0200

    random stuff

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -12,6 +12,7 @@ func _process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
 		direction = -1
 		animated_sprite.flip_h = true
+		
 	if ray_cast_left.is_colliding():
 		direction = 1
 		animated_sprite.flip_h = false
