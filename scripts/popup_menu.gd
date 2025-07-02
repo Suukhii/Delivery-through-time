41ecda3e4e2c43e76c08bddfb9d3aeb412cda90e
TheMB04 <122024164+TheMB04@users.noreply.github.com>
2025-07-02 23:54:43 +0200

    lots of options

--- scripts/popup_menu.gd
+++ scripts/popup_menu.gd
@@ -1,11 +1,38 @@
 extends MarginContainer
 
+@export var menu_screen: VBoxContainer
+@export var open_menu_screen: MarginContainer
+@export var open_options_screen: MarginContainer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
+func toggle_visibility(object):
+	if object.visible:
+		object.visible = false
+	else:
+		object.visible = true
+		
+
+func _input(event):
+	if event.is_action_pressed("pauseOrSettings"):
+		toggle_visibility(menu_screen)
+		toggle_visibility(open_menu_screen)
+		if open_options_screen.visible:
+			toggle_visibility(open_options_screen)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_settings_pressed() -> void:
+	toggle_visibility(menu_screen)
+	toggle_visibility(open_menu_screen)
+
+
+func _on_resume_pressed() -> void:
+	toggle_visibility(menu_screen)
+	toggle_visibility(open_menu_screen)
+	
+	
+func _on_options_pressed() -> void:
+	toggle_visibility(open_options_screen)
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
