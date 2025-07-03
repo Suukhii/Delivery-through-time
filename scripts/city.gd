163f099efd47b83de5a817fc1e0a4261398472d2
Suukhii <sukhmanikakar11@gmail.com>
2025-07-03 13:06:48 +0200

    fixed heart container and spawnpoint

--- scripts/city.gd
+++ scripts/city.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-@export var player_scene: PackedScene  # Assign your player scene in the Inspector
-
 func _ready():
-	var player = player_scene.instantiate()
-	add_child(player)
-
 	var spawn_point = $PlayerSpawn
-	player.global_position = spawn_point.global_position
+	var player = $Player  # Direct reference to the Player node in the scene
+	
+	if player and spawn_point:
+		player.global_position = spawn_point.global_position
+	else:
+		print("Player or spawn point not found!")
--- scripts/heart_container.gd
+++ scripts/heart_container.gd
@@ -3,13 +3,15 @@ extends Control
 @onready var hearts = get_children()  # Gets the 3 TextureRects
 
 func _ready():
-	var players = get_tree().get_nodes_in_group("Player")
+	var players = get_tree().get_nodes_in_group("Player")  # Searches globally
 	if players.size() > 0:
 		var player = players[0]
 		player.connect("hit", Callable(self, "_on_player_hit"))
+		print("Connected to player's hit signal")
 	else:
-		print("Player not found!")
+		print("Player not found in group!")
 	_reset_hearts()
+
 
 
 func _on_player_hit(lives_left):
